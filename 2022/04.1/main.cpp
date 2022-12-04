79a2a5fcec197bf3b3402292c50767b99989eae4
Arturas Lapinskas <arturaslape@gmail.com>
2022-12-04 15:36:25 +0100

    4th day solutions.

---
+++ 2022/04.1/main.cpp
@@ -0,0 +1,28 @@
+#include <cassert>
+#include <iostream>
+#include <regex>
+#include <sstream>
+using namespace std;
+typedef pair<int, int> pii;
+
+pair<pii, pii> parse(const string &s) {
+  static const regex re("(\\d+)-(\\d+),(\\d+)-(\\d+)");
+  smatch m;
+  assert(regex_match(s, m, re));
+  return {{stoi(m[1]), stoi(m[2])}, {stoi(m[3]), stoi(m[4])}};
+}
+
+bool is_sub(const pii &l, const pii &r) {
+  return r.first <= l.first && l.second <= r.second;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(0);
+  int r = 0;
+  for (string line; getline(cin, line);) {
+    const auto [lhs, rhs] = parse(line);
+    r += is_sub(lhs, rhs) || is_sub(rhs, lhs);
+  }
+  cout << r << endl;
+  return 0;
+}
