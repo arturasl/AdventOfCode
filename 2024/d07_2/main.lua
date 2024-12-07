6ec7182d2fb2142dfdfa2a9782e848c894feee0e
Arturas Lapinskas <arturaslape@gmail.com>
2024-12-07 21:45:14 +0100

    Day 7.

---
+++ 2024/d07_2/main.lua
@@ -0,0 +1,32 @@
+local result = 0
+for line in io.lines() do
+	line = line:gsub("^%s+", ""):gsub("%s+$", "")
+	if line ~= "" then
+		local str_expected, rest = line:match("(%d+):(.*)")
+		local expected = tonumber(str_expected)
+
+		local nums = {}
+		for str_num in rest:gmatch("%d+") do
+			nums[#nums + 1] = tonumber(str_num)
+		end
+
+		local dp = { [1] = { [nums[1]] = 1 } }
+
+		for i = 2, #nums do
+			dp[i] = {}
+			for prev, cnt in pairs(dp[i - 1]) do
+				local function update(val)
+					dp[i][val] = (dp[i][val] or 0) + cnt
+				end
+
+				update(prev + nums[i])
+				update(prev * nums[i])
+				update(tonumber(string.format("%d%d", prev, nums[i])))
+			end
+		end
+
+		result = result + (dp[#nums][expected] ~= nil and expected or 0)
+	end
+end
+
+print(result)
