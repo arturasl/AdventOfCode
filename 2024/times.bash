6c0b922bbee4ce76e175710b0cdfe0e25486c66b
Arturas Lapinskas <arturaslape@gmail.com>
2024-12-25 13:47:59 +0100

    Giving up and starting to use luajit.

--- 2024/times.bash
+++ 2024/times.bash
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+declare -rA jitted=(["d14_2"]=1 ["d24_2"]=1)
+
 time=/usr/bin/time
 above_1s=0
-while IFS= read -r prog || [[ -n "$prog" ]]; do
-    day="$(basename "$(dirname "${prog}")")"
-    output="$($time --format '%e' lua "$prog" < "./${day}/large.in" 2>&1)"
+while IFS= read -r main_file || [[ -n "$main_file" ]]; do
+    day="$(basename "$(dirname "${main_file}")")"
+
+    program="lua"
+    if [[ -n "${jitted["$day"]}" ]] ; then
+        program="luajit"
+    fi
+
+    output="$($time --format '%e' $program "$main_file" < "./${day}/large.in" 2>&1)"
     elapsed_sec=$(echo "$output" | tail -n 1)
     if [[ ! "$elapsed_sec" =~ ^0\. ]]; then
         above_1s=$((above_1s + 1))
     fi
     result=$(echo "$output" | tail -n 2 | head -n 1)
-    echo "${day}, elapsed_sec: ${elapsed_sec}s, result: ${result}"
+
+    echo "${program} ${day}, elapsed_sec: ${elapsed_sec}s, result: ${result}"
 done < <(find . -name 'main.lua' | sort)
 
 echo "Above 1s: ${above_1s}"
