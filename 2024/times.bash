20142b6a114943629e1a2607963fa4c7b56ad9d6
Arturas Lapinskas <arturaslape@gmail.com>
2024-12-25 11:15:17 +0100

    Include amount of tasks whose execution took more than 1s.

--- 2024/times.bash
+++ 2024/times.bash
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 time=/usr/bin/time
+above_1s=0
 while IFS= read -r prog || [[ -n "$prog" ]]; do
     day="$(basename "$(dirname "${prog}")")"
     output="$($time --format '%e' lua "$prog" < "./${day}/large.in" 2>&1)"
     elapsed_sec=$(echo "$output" | tail -n 1)
+    if [[ ! "$elapsed_sec" =~ ^0\. ]]; then
+        above_1s=$((above_1s + 1))
+    fi
     result=$(echo "$output" | tail -n 2 | head -n 1)
     echo "${day}, elapsed_sec: ${elapsed_sec}s, result: ${result}"
 done < <(find . -name 'main.lua' | sort)
+
+echo "Above 1s: ${above_1s}"
