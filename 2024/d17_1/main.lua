f46d1bfd9c7e50b50221d97838afaec57788b7a2
Arturas Lapinskas <arturaslape@gmail.com>
2024-12-26 10:39:24 +0100

    Print execution steps.

--- 2024/d17_1/main.lua
+++ 2024/d17_1/main.lua
@@ -1,5 +1,3 @@
-local inspect = require("inspect")
-
 local function read()
     local data = { program = {}, registers = {}, pointer = 0 }
     for line in io.lines() do
@@ -22,56 +20,79 @@ local function read()
     return data
 end
 
-local function calc_combo_val(num, data)
+local function join_arr(arr, sep)
+    local result = ""
+    for idx, val in ipairs(arr) do
+        if idx ~= 1 then
+            result = result .. sep
+        end
+        result = result .. tostring(val)
+    end
+    return result
+end
+
+local function calc_combo_str(num)
     if 0 <= num and num <= 3 then
-        return num
+        return tostring(num)
     end
     if 4 <= num and num <= 6 then
-        local val = data.registers[string.char(string.byte("A") + (num - 4))]
-        assert(val ~= nil)
-        return val
+        return string.char(string.byte("A") + (num - 4))
     end
 
     assert(false, num)
 end
 
+local function calc_combo_val(str, data)
+    if str:match("^%d+$") then
+        return tonumber(str)
+    end
+    local val = data.registers[str]
+    assert(val ~= nil)
+    return val
+end
+
 local function main()
     local data = read()
-    local first_print = true
 
+    local result = {}
     while data.pointer < #data.program do
         assert(data.pointer + 1 < #data.program)
         assert(0 <= data.pointer)
 
         local op = data.program[data.pointer + 1]
         local val = data.program[data.pointer + 2]
-        local combo = calc_combo_val(val, data)
+        local str_combo = calc_combo_str(val)
+        local combo = calc_combo_val(str_combo, data)
         if op == 0 then
             assert(combo >= 0)
-            data.registers["A"] = data.registers["A"] // (1 << combo)
+            data.registers["A"] = data.registers["A"] >> combo
+            print(("A = A >> %s"):format(str_combo))
         elseif op == 1 then
             data.registers["B"] = data.registers["B"] ~ val
+            print(("B = B xor %d"):format(val))
         elseif op == 2 then
-            data.registers["B"] = (combo % 8)
+            data.registers["B"] = combo & 7
+            print(("B = %s & 0b111"):format(str_combo))
         elseif op == 3 then
             if data.registers["A"] ~= 0 then
                 assert(val % 2 == 0)
                 data.pointer = val - 2
             end
+            print(("jnz A to %d"):format(val))
         elseif op == 4 then
             data.registers["B"] = data.registers["B"] ~ data.registers["C"]
+            print("B = B xor C")
         elseif op == 5 then
-            if not first_print then
-                io.write(",")
-            end
-            first_print = false
-            io.write(combo % 8)
+            result[#result + 1] = combo & 7
+            print(("print %s & 0b111"):format(str_combo))
         elseif op == 6 then
             assert(combo >= 0)
-            data.registers["B"] = data.registers["A"] // (1 << combo)
+            data.registers["B"] = data.registers["A"] >> combo
+            print(("B = A >> %s"):format(str_combo))
         elseif op == 7 then
             assert(combo >= 0)
-            data.registers["C"] = data.registers["A"] // (1 << combo)
+            data.registers["C"] = data.registers["A"] >> combo
+            print(("C = A >> %s"):format(str_combo))
         else
             assert(false)
         end
@@ -79,7 +100,7 @@ local function main()
         data.pointer = data.pointer + 2
     end
 
-    io.write("\n")
+    print(join_arr(result, ","))
 end
 
 main()
