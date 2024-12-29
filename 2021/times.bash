54838cf348ca8afed42befb579c801eabfd3ef15
Arturas Lapinskas <arturaslape@gmail.com>
2024-12-29 20:47:46 +0100

    Times file for 2021.

---
+++ 2021/times.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+time=/usr/bin/time
+above_1s=0
+while IFS= read -r main_file || [[ -n "$main_file" ]]; do
+    day="$(basename "$(dirname "${main_file}")")"
+
+    program='cargo'
+    output="$($time --format '%e' $program run --bin "$day" < "./src/${day}/large.in" 2>&1)"
+    elapsed_sec=$(echo "$output" | tail -n 1)
+    if [[ ! "$elapsed_sec" =~ ^0\. ]]; then
+        above_1s=$((above_1s + 1))
+    fi
+    result=$(echo "$output" | tail -n 2 | head -n 1)
+
+    echo "${program} ${day}, elapsed_sec: ${elapsed_sec}s, result: ${result}"
+done < <(find . -name 'main.rs' | sort)
+
+echo "Above 1s: ${above_1s}"
