9b96b0812e678025f9313e7dfb9911aaec4d8d26
Arturas Lapinskas <arturaslape@gmail.com>
2025-01-05 09:58:29 +0100

    Trying to be more precise on day 17 on which elements are reachable vs not.

--- 2021/src/d17_2/main.rs
+++ 2021/src/d17_2/main.rs
@@ -1,6 +1,9 @@
+use ahash::AHashSet;
 use anyhow::{Context, Ok, Result};
 use regex::Regex;
+use std::collections::BTreeMap;
 use std::io::{self, BufRead};
+use std::ops::Bound::{Included, Unbounded};
 use std::thread;
 
 fn run() -> Result<()> {
@@ -23,26 +26,49 @@ fn run() -> Result<()> {
             captures["ymax"].parse::<i64>()?,
         );
 
-        let mut dist = 0;
-        for vx in 0..xma + 1 {
-            for vy in ymi..ymi.abs() + 1 {
-                let (mut lvy, mut lvx) = (vy, vx);
-                let (mut sx, mut sy) = (0, 0);
-                let mut ok = false;
-                while sy >= ymi && sx <= xma {
-                    sx += lvx;
-                    sy += lvy;
-                    lvx = (lvx - 1).max(0);
-                    lvy -= 1;
-                    if xmi <= sx && sx <= xma && ymi <= sy && sy <= yma {
-                        ok = true;
-                    }
+        let mut ok_vys: BTreeMap<i64, Vec<i64>> = BTreeMap::new();
+        for vy in ymi..ymi.abs() {
+            let (mut sy, mut lvy) = (0, vy);
+            let mut time = 0;
+            while sy >= ymi {
+                sy += lvy;
+                lvy -= 1;
+                if ymi <= sy && sy <= yma {
+                    ok_vys.entry(time).or_default().push(vy);
                 }
-                dist += i64::from(ok);
+                time += 1;
             }
         }
 
-        println!("{}", dist);
+        let mut result = 0;
+        let mut xstart = 0;
+        while xstart * (1 + xstart) < 2 * xmi {
+            xstart += 1;
+        }
+
+        for vx in xstart..xma + 1 {
+            let (mut sx, mut lvx) = (0, vx);
+            let mut time = 0;
+            let mut hit_ys: AHashSet<i64> = AHashSet::new();
+            while sx <= xma && lvx != 0 {
+                sx += lvx;
+                lvx = (lvx - 1).max(0);
+                if xmi <= sx && sx <= xma {
+                    if lvx == 0 {
+                        for (_, vys) in ok_vys.range((Included(time), Unbounded)) {
+                            hit_ys.extend(vys);
+                        }
+                    } else if let Some(vys) = ok_vys.get(&time) {
+                        hit_ys.extend(vys);
+                    }
+                }
+                time += 1;
+            }
+
+            result += hit_ys.len();
+        }
+
+        println!("{}", result);
     }
     Ok(())
 }
