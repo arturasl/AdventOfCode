dc2108c1a7ccf87ac29f8a67d6d3e375d412a4ca
Arturas Lapinskas <arturaslape@gmail.com>
2025-01-05 08:32:49 +0100

    Pure math solution to day 17 part 1.

--- 2021/src/d17_1/main.rs
+++ 2021/src/d17_1/main.rs
@@ -13,35 +13,8 @@ fn run() -> Result<()> {
         }
 
         let captures = re_inp.captures(&line).context("")?;
-        let (xmi, xma) = (
-            captures["xmin"].parse::<i64>()?,
-            captures["xmax"].parse::<i64>()?,
-        );
-        let (ymi, yma) = (
-            captures["ymin"].parse::<i64>()?,
-            captures["ymax"].parse::<i64>()?,
-        );
-
-        let mut by = 0;
-        for vx in 0..xma + 1 {
-            for vy in ymi..ymi.abs() + 1 {
-                let (mut lvy, mut lvx) = (vy, vx);
-                let (mut sx, mut sy) = (0, 0);
-                let mut local_by = 0;
-                while sy >= ymi && sx <= xma {
-                    sx += lvx;
-                    sy += lvy;
-                    lvx = (lvx - 1).max(0);
-                    lvy -= 1;
-                    local_by = local_by.max(sy);
-                    if xmi <= sx && sx <= xma && ymi <= sy && sy <= yma {
-                        by = by.max(local_by);
-                    }
-                }
-            }
-        }
-
-        println!("{}", by);
+        let ymi = captures["ymin"].parse::<i64>()?;
+        println!("{}", (ymi * (ymi + 1)) / 2);
     }
     Ok(())
 }
