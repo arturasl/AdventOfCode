4d1fca8c546d53d7020ba8f3ce5af998bd22b339
Arturas Lapinskas <arturaslape@gmail.com>
2025-05-10 15:48:47 +0200

    Day 10.

---
+++ 2020/10.01/main.py
@@ -0,0 +1,25 @@
+import sys
+from collections import defaultdict
+
+
+def main():
+    nums = []
+    for line in sys.stdin:
+        line = line.strip()
+        if not line:
+            continue
+        nums.append(int(line))
+
+    nums.append(0)
+    nums.append(max(nums) + 3)
+    nums = sorted(nums)
+    diffs = defaultdict(int)
+    for i in range(1, len(nums)):
+        diffs[nums[i] - nums[i - 1]] += 1
+
+    assert 0 <= max(diffs.keys()) <= 3
+    print(diffs[1] * diffs[3])
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2020/10.02/main.py
@@ -0,0 +1,35 @@
+import sys
+
+from cachetools import cached
+from cachetools.keys import hashkey
+
+
+@cached(cache={}, key=lambda _, idx, prev_val: hashkey((idx, prev_val)))
+def count(nums: list[int], idx: int, prev_val: int):
+    if len(nums) == idx:
+        return 1
+
+    ans = 0
+    while idx < len(nums) and nums[idx] - prev_val <= 3:
+        ans += count(nums, idx + 1, nums[idx])
+        idx += 1
+
+    return ans
+
+
+def main():
+    nums: list[int] = []
+    for line in sys.stdin:
+        line = line.strip()
+        if not line:
+            continue
+        nums.append(int(line))
+
+    nums.append(0)
+    nums.append(max(nums) + 3)
+    nums = sorted(nums)
+    print(count(nums, 1, nums[0]))
+
+
+if __name__ == "__main__":
+    main()
