9a551089b15a10753884f8ce7891c66914f6cd54
arturasl <589741+arturasl@users.noreply.github.com>
2025-11-03 12:52:51 +0100

    A small script to get a new project based on the previous one.

---
+++ 2019/cp.bash
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+shopt -s failglob
+
+main() {
+    local cur_dir
+    cur_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+    local newest
+    newest="$(find  "$cur_dir" -maxdepth 1 -name "d*_*" | sort --reverse | head --lines=1)"
+
+    local from="${1-}"
+    if [[ -z "$from" ]]; then
+        from="$newest"
+    fi
+    local from_base
+    from_base="$(basename "$from")" # d05_1
+
+    local newest_base
+    newest_base="$(basename "$newest")"
+    local newest_day
+    newest_day="$(echo "$newest_base" | cut --delimiter=_ --fields=1 | sed "--expression=s#^d0*##")"
+    local newest_task
+    newest_task="$(echo "$newest_base" | cut --delimiter=_ --fields=2)"
+
+    local next_day="$newest_day"
+    local next_task="$newest_task"
+    if [[ "$next_task" == "2" ]]; then
+        next_day="$(( next_day + 1))"
+        next_task=1
+    else
+        next_task=2
+    fi
+    local next_base
+    next_base="$(printf "d%02d_%d" "$next_day" "$next_task")"
+
+    cp --recursive "$from" "${cur_dir}/${next_base}"
+    mv "${cur_dir}/${next_base}/src/${from_base}" "${cur_dir}/${next_base}/src/${next_base}"
+    find "${cur_dir}/${next_base}" -type f -exec \
+        sed --in-place "--expression=s#${from_base}#${next_base}#g" '{}' \
+        ';'
+
+    local from_base_dash
+    from_base_dash=$(echo "$from_base" | tr '_' '-')
+    local next_base_dash
+    next_base_dash=$(echo "$next_base" | tr '_' '-')
+    find "${cur_dir}/${next_base}" -type f -exec \
+        sed --in-place "--expression=s#${from_base_dash}#${next_base_dash}#g" '{}' \
+        ';'
+}
+
+main "$@"
