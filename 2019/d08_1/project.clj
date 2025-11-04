18bc07f83a6e4a3980af12d604456741a35d4ed5
arturasl <589741+arturasl@users.noreply.github.com>
2025-11-04 09:42:37 +0100

    Day 8 part 1.

---
+++ 2019/d08_1/project.clj
@@ -0,0 +1,12 @@
+(defproject d04_1 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.12.3"]
+                 [org.clojure/math.combinatorics "0.3.0"]]
+  :main ^:skip-aot d08-1.core
+  :target-path "target/%s"
+  :test-paths ["src/" "test/"]
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ 2019/d08_1/src/d08_1/core.clj
@@ -0,0 +1,42 @@
+(ns d08-1.core
+  (:gen-class)
+  (:require [clojure.string :as str]
+            [clojure.test :refer [deftest is]]))
+
+(def width 25)
+(def height 6)
+
+(defn str->list [s]
+  (->> (map str s)
+       (map str/trim)
+       (remove empty?)
+       (map parse-long)
+       (into [])))
+
+(deftest test-str->list
+  (is (= [1 2 3] (str->list "12\n3 "))))
+
+(defn find-layer-frequencies [lst w h]
+  (->> (partition (* w h) lst)
+       (map frequencies)))
+
+(deftest test-find-layer-frequencies
+  (is (= [{1 2, 3 1, 4 1, 5 1, 6 1} {7 1, 8 1, 9 1, 0 1, 1 1, 2 1}]
+         (find-layer-frequencies (str->list "113456789012") 3 2))))
+
+(defn find-best-layer [lst w h]
+  (->>  (find-layer-frequencies lst w h)
+        (apply min-key #(get % 0 0))
+        (into {})))
+
+(deftest test-find-best-layer
+  (is (= [{1 2, 3 1, 4 1, 5 1, 6 1}]
+         (find-best-layer (str->list "113456789012") 3 2))))
+
+(defn solve [s]
+  (let [layer (find-best-layer (str->list s) width height)]
+    (* (get layer 1) (get layer 2))))
+
+(defn -main
+  [& _]
+  (println (solve (slurp  "./large.in"))))
