19645c7714c9c9e002529619e6fa158165b675d1
arturasl <589741+arturasl@users.noreply.github.com>
2025-11-16 17:59:35 +0100

    2025 day 1.

---
+++ 2025/d01_1/index.js
@@ -0,0 +1,21 @@
+import readline from "node:readline";
+import assert from "node:assert";
+
+async function main() {
+  let position = 50;
+  let zeroes = 0;
+  for await (const line of readline.createInterface({
+    input: process.stdin,
+  })) {
+    const match = line.match(/^(?<dir>[LR])(?<str_ammount>\d+)$/);
+    assert(match, `While parsing ${line}`);
+    const { dir, str_ammount } = match.groups;
+    position += (dir == "R" ? +1 : -1) * parseInt(str_ammount, 10);
+    position = ((position % 100) + 100) % 100;
+    zeroes += position == 0;
+  }
+
+  console.log(zeroes);
+}
+
+main();
---
+++ 2025/d01_2/index.js
@@ -0,0 +1,46 @@
+import readline from "node:readline";
+import assert from "node:assert";
+
+function mod(x, m) {
+  return ((x % m) + m) % m;
+}
+
+function div(a, b) {
+  return Math.trunc(a / b);
+}
+
+async function main() {
+  let position = 50;
+  let zeroes = 0;
+  for await (const line of readline.createInterface({
+    input: process.stdin,
+  })) {
+    const match = line.match(/^(?<dir>[LR])(?<str_ammount>\d+)$/);
+    assert(match, `While parsing ${line}`);
+    const { dir, str_ammount } = match.groups;
+
+    const next_position =
+      position + (dir == "R" ? +1 : -1) * parseInt(str_ammount, 10);
+
+    if (next_position % 100 == 0) {
+      zeroes += 1;
+    }
+
+    let [lhs, rhs] = [position, next_position];
+    if (rhs < lhs) {
+      [lhs, rhs] = [rhs, lhs];
+    }
+
+    lhs = lhs + 100 - mod(lhs, 100);
+
+    if (lhs < rhs) {
+      zeroes += 1 + div(rhs - lhs - 1, 100);
+    }
+
+    position = mod(next_position, 100);
+  }
+
+  console.log(zeroes);
+}
+
+main();
