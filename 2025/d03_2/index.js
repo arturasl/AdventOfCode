adaafd18830c4545c5fc83d5b0d49159c40fd1d6
arturasl <589741+arturasl@users.noreply.github.com>
2025-12-03 07:28:38 +0100

    Small change: remove unnecessary slice.

--- 2025/d03_2/index.js
+++ 2025/d03_2/index.js
@@ -1,31 +1,35 @@
 import readline from "node:readline";
+import assert from "node:assert";
 
-function maxi(arr) {
-  const pos = arr.reduce(
-    (best_idx, cur_val, cur_idx) =>
-      best_idx == null || arr[best_idx] < cur_val ? cur_idx : best_idx,
-    null,
-  );
-  return { pos, val: pos == null ? "" : arr[pos] };
+function maxi(arr, start, end) {
+  assert(start < end);
+  let best_idx = start;
+  for (let idx = start + 1; idx < end; idx += 1) {
+    if (arr[best_idx] < arr[idx]) {
+      best_idx = idx;
+    }
+  }
+  return best_idx;
 }
 
 async function main() {
+  const find_of_len = 12;
   let sum = 0n;
   for await (const line of readline.createInterface({
     input: process.stdin,
   })) {
-    const arr = Array.from(line);
-    let result = [];
+    let result = "";
     let start = 0;
-    for (let i = 12; i > 0; i -= 1) {
-      const { pos, val } = maxi(arr.slice(start, arr.length - i + 1));
-      start += pos + 1;
-      result.push(val);
+    assert(line.length >= find_of_len);
+    for (let i = find_of_len; i > 0; i -= 1) {
+      const pos = maxi(line, start, line.length - i + 1);
+      start = pos + 1;
+      result += line[pos];
     }
 
-    sum += BigInt(result.join(""));
+    sum += BigInt(result);
   }
-  console.log(sum);
+  console.log(sum.toString());
 }
 
 main();
