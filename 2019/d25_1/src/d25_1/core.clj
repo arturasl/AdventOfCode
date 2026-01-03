22f23491222f69ce4eccc906878141aa83b9662c
arturasl <589741+arturasl@users.noreply.github.com>
2026-01-03 21:40:56 +0100

    Handle security check and cache based on name of the room.

--- 2019/d25_1/src/d25_1/core.clj
+++ 2019/d25_1/src/d25_1/core.clj
@@ -29,48 +29,36 @@
 (defn get-str-output [program]
   (str/join "" (map char (:output program))))
 
-(defn add-coords [lhs rhs]
-  (merge-with + lhs rhs))
-
 (defn robot-move [orig-program dir]
   (assert (or (nil? dir)
               (contains? #{:east :west :north :south} dir))
           (str dir))
   (let [program (if (nil? dir)
                   (-> orig-program
-                      (assoc :room {:coords {:y 0 :x 0}})
                       (assoc :robot {:items #{}}))
                   (str-to->stdin orig-program (name dir)))
-        str-output (get-str-output program)]
-    (if (not (nil? (str/index-of str-output "and you are ejected back to the checkpoint")))
-      orig-program
-      (let [matches (zipmap [:name :desc :doors :items]
-                            (rest (re-matches
-                                   (re-pattern
-                                    (str
-                                     "(?s)^\n*"
-                                     "(== [^=]+ ==)\n+"
-                                     "([^\n]+)\n+"
-                                     "(Doors here lead:\n(?:[^\n]+\n)+)?\n*"
-                                     "(Items here:\n(?:[^\n]+\n)+)?\n*"
-                                     "Command\\?\n+$"))
-                                   str-output)))
-            _ (assert (not (nil? (:name matches))) str-output)
-            parsed-parts (-> matches
-                             (update :name #(str/replace % #"^=+ *| *=+$" ""))
-                             (update :doors str-list-to-set)
-                             (update :doors #(into #{} (map keyword %)))
-                             (update :items str-list-to-set))
-            coords (add-coords (get-in program [:room :coords])
-                               (get {:north {:y -1 :x 0}
-                                     :south {:y 1 :x 0}
-                                     :east {:y 0 :x 1}
-                                     :west {:y 0 :x -1}}
-                                    dir))]
-        (-> program
-            (assoc :output (finger/double-list))
-            (assoc :room parsed-parts)
-            (assoc-in [:room :coords] coords))))))
+        str-output (get-str-output program)
+        str-output (str/replace str-output #"(?s).*and you are ejected back to the checkpoint.*?\n" "")
+        matches (zipmap [:name :desc :doors :items]
+                        (rest (re-matches
+                               (re-pattern
+                                (str
+                                 "(?s)^\n*"
+                                 "(== [^=]+ ==)\n+"
+                                 "([^\n]+)\n+"
+                                 "(Doors here lead:\n(?:[^\n]+\n)+)?\n*"
+                                 "(Items here:\n(?:[^\n]+\n)+)?\n*"
+                                 "Command\\?\n+$"))
+                               str-output)))
+        _ (assert (not (nil? (:name matches))) str-output)
+        parsed-parts (-> matches
+                         (update :name #(str/replace % #"^=+ *| *=+$" ""))
+                         (update :doors str-list-to-set)
+                         (update :doors #(into #{} (map keyword %)))
+                         (update :items str-list-to-set))]
+    (-> program
+        (assoc :output (finger/double-list))
+        (assoc :room parsed-parts))))
 
 (defn robot-take [program item]
   (let [room-has (get-in program [:room :items])]
@@ -83,7 +71,9 @@
                    ; :/
                    "infinite loop"
                    ; It is suddenly completely dark! You are eaten by a Grue!
-                   "photons"}
+                   "photons"
+                   ; The giant electromagnet is stuck to you.  You can't move!!
+                   "giant electromagnet"}
                  item) program
       (let [program (str-to->stdin program (str "take " item))
             str-output (get-str-output program)
@@ -109,7 +99,9 @@
             _ (assert (= (:took matches) item))]
         (-> program
             (assoc :output (finger/double-list))
-            (update-in [:room :items] conj item)
+            ; If we drop, lets not try to pick it back as otherwise state key
+            ; becomes very large.
+            ; (update-in [:room :items] conj item)
             (update-in [:robot :items] disj item)))))
 
 (defn robot-ensure-has [program items]
@@ -139,23 +131,53 @@
     with-items))
 
 (defn program->grid-key [program]
-  {:y (get-in program [:room :coords :y])
-   :x (get-in program [:room :coords :x])
-   :items (get-in program [:robot :items])})
+  {:name (get-in program [:room :name])
+   :robot-items (get-in program [:robot :items])})
 
 (defn programs->grid [grid programs]
   (->> programs
        (map #(vector (program->grid-key %) %))
        (into grid)))
 
+(defn print-grid-stats [grid]
+  (let [uniq-msgs (sort
+                   (distinct
+                    (map #(get-in % [:room :desc])
+                         (vals grid))))
+        uniq-items (sort
+                    (distinct
+                     (sequence
+                      cat
+                      (map #(get-in % [:room :items])
+                           (vals grid)))))
+        uniq-rooms (sort
+                    (distinct
+                     (map #(get-in % [:room :name])
+                          (vals grid))))]
+    (println "")
+    (println "### Unique messages (" (count uniq-msgs) ")")
+    (doseq [msg uniq-msgs]
+      (println msg))
+
+    (println "")
+    (println "### Unique items (" (count uniq-items) ")")
+    (doseq [item uniq-items]
+      (println item))
+
+    (println "")
+    (println "### Unique rooms (" (count uniq-rooms) ")")
+    (doseq [room uniq-rooms]
+      (println room))))
+
 (defn search [starts]
   (loop [search-space (into (finger/double-list) starts)
          grid (programs->grid {} starts)
          its 1]
-    (when (zero? (mod its 100))
+    (when (zero? (mod its 500))
       (println "Its:" its
                "Search space:" (count search-space)
-               "Visited:" (count grid)))
+               "Visited:" (count grid))
+      (print-grid-stats grid))
     (if (empty? search-space) {:grid grid}
         (let [cur (first search-space)
               its (inc its)
@@ -174,28 +196,7 @@
                     code/exec
                     (robot-move nil))
         grid (:grid (search [program]))]
-    (println "")
-    (println "### Unique messages")
-    (doseq [msg (distinct
-                 (map #(get-in % [:room :desc])
-                      (vals grid)))]
-      (println msg))
-
-    (println "")
-    (println "### Unique items")
-    (doseq [item (distinct
-                  (sequence
-                   cat
-                   (map #(get-in % [:room :items])
-                        (vals grid))))]
-      (println item))
-
-    (println "")
-    (println "### Unique rooms")
-    (doseq [item (distinct
-                  (map #(get-in % [:room :name])
-                       (vals grid)))]
-      (println item))))
+    (print-grid-stats grid)))
 
 (defn -main
   [& _]
