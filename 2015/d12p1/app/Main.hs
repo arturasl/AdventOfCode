5b186aa619b17afcd1d57cc1574b8e918a75168e
arturasl <589741+arturasl@users.noreply.github.com>
2026-05-09 08:21:34 +0200

    Day 12 part 1.

---
+++ 2015/d12p1/app/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Debug.Trace (traceShow)
+import Text.Regex.TDFA (getAllTextMatches, (=~))
+
+trc :: (Show a) => a -> a
+trc x = traceShow x x
+
+solve :: T.Text -> Int
+solve ln = sum $ map read strNums
+  where
+    strNums = getAllTextMatches (T.unpack ln =~ ("-?[0-9]+" :: T.Text)) :: [String]
+
+main :: IO ()
+main = do
+  contents <- TIO.getContents
+  let lns = filter (not . T.null) . map T.strip $ T.lines contents
+  print $ map solve lns
